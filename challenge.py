2f2aa403efc6026a64d0a76936922256cc4f044e
Cam Wade <64671506+cbztech@users.noreply.github.com>
2020-05-22 11:37:34 -0600

    Create challenge.py

---
+++ challenge.py
@@ -0,0 +1,24 @@
+from base64 import *
+
+with open('C:\encodedflag.txt', 'r') as encoded_file:
+    decode = encoded_file.read()
+    loop_count = 1
+
+    while loop_count < 15:
+            if loop_count <= 5:
+                # Base 16 decoding
+                while loop_count <= 5:
+                    decode = b16decode(decode)
+                    loop_count +=1
+            elif loop_count > 5 and loop_count <= 10:
+                # Base 32 decoding
+                while loop_count <= 10:
+                    decode = b32decode(decode)
+                    loop_count +=1
+            elif loop_count > 10 and loop_count <= 15:
+                # Base 64 decoding
+                while loop_count <= 15:
+                    decode = b64decode(decode)
+                    loop_count +=1
+
+    print('The final decoded string is :\n' + str(decode))
